25640fa3558174298bf389634212de4acec61e47
Eduardo-Malhao <eduardo.malhao@outlook.com>
2023-10-17 16:28:03 -0300

    REQ 07

---
+++ src/main/java/com/betrybe/museumfinder/advice/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.betrybe.museumfinder.advice;
+
+import com.betrybe.museumfinder.exception.InvalidCoordinateException;
+import com.betrybe.museumfinder.exception.MuseumNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(MuseumNotFoundException.class)
+  public ResponseEntity<String> handleResourceNotFound(MuseumNotFoundException ex) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Museu não encontado!");
+  }
+
+  @ExceptionHandler(InvalidCoordinateException.class)
+  public ResponseEntity<String> handleServerError(InvalidCoordinateException ex) {
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Coordenada inválida!");
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<String> handleBadRequest(Exception ex) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro interno!");
+  }
+}
+

80057d7cde1d2d64952a9a40175d3c81adb9d391
Eduardo-Malhao <eduardo.malhao@outlook.com>
2023-10-17 19:58:55 -0300

    Testes

--- src/test/java/com/betrybe/museumfinder/solution/MuseumTest.java
+++ src/test/java/com/betrybe/museumfinder/solution/MuseumTest.java
@@ -3,7 +3,9 @@ package com.betrybe.museumfinder.solution;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import com.betrybe.museumfinder.dto.CollectionTypeCount;
+import com.betrybe.museumfinder.model.Museum;
 import com.betrybe.museumfinder.service.CollectionTypeService;
+import com.betrybe.museumfinder.service.MuseumService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -13,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import java.util.Optional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -23,6 +26,7 @@ public class MuseumTest {
 
   @MockBean
   CollectionTypeService collectionService;
+  MuseumService museumService;
 
   @Test
   @DisplayName("Requisicao BAD")
@@ -44,5 +48,30 @@ public class MuseumTest {
 
     mockMvc.perform(get("http://localhost:8080/collections/count/louvre").accept(MediaType.APPLICATION_JSON)).
         andExpect(status().isOk());
+  }
+
+  @Test
+  @DisplayName("Requisicao OK - Museu encontrado")
+  void getMuseumByIdFound() throws Exception {
+    Long id = 1L;
+    Museum museum = new Museum();
+    Mockito
+      .when(museumService.getMuseum(id))
+        .thenReturn(Optional.of(museum));
+
+    mockMvc.perform(get("http://localhost:8080/museums/{id}", id).accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  @DisplayName("Requisicao - Museu nao encontrado")
+  void getMuseumByIdNotFound() throws Exception {
+    Long id = 2L;
+    Mockito
+    .when(museumService.getMuseum(id))
+        .thenReturn(Optional.empty());
+
+    mockMvc.perform(get("http://localhost:8080/museums/{id}", id).accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isNotFound());
   }
 }
